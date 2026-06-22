9475c9f3ee8f816acbcaa1655b7772881cfb092a
bikrant0 <shilpakarbikrant@gmail.com>
2026-06-22 19:39:41 +0545

    SECURITY PATCH: Locked down ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -10,13 +10,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7+_-y@i6c@6imoct#9lcp6_fkl%ve9&z_f=6bq^#@4tenlyax1'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dummy-key-for-local-dev-only')
+
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'enterprise-task-api-django.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
