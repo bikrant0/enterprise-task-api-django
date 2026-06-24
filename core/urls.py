c1868668d9ac6368e9a3977807924f4985ceba97
bikrant0 <shilpakarbikrant@gmail.com>
2026-06-24 14:13:45 +0545

    Added root URL welcome message

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import JsonResponse
+
+def api_welcome(request):
+    return JsonResponse({
+        "message": "Welcome to the Enterprise Task Manager API!",
+        "status": "Production server is running smoothly.",
+        "docs": "End points are available at /api/tasks/ and authentication at /api/auth/login/ and /api/auth/refresh/"
+        })
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/tasks/', include('tasks.urls')),
-    
+    path('', api_welcome, name='api_welcome'),
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
